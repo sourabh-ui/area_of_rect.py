6d15c9dd3221ae5df90a59a66ce82a9636fb6356
sourabh-ui <sourabhhartale85@gmail.com>
2024-07-04 12:47:28 +0530

    Update area_of_rect.py

--- area_of_rect.py
+++ area_of_rect.py
@@ -1,5 +1,5 @@
-length_of_rect=int(input(enter the length of rectangle)
-length_of_rect=int(input(enter the length of rectangle)
+length_of_rect=int(input(enter the length of rectangle))
+length_of_rect=int(input(enter the length of rectangle))
 area_of_rect=length*breadth
 print("length_of_rect",length"breadth_of_rect",breadth"area_of_rect",area)
 print(f"length_of_rect",{length}"breadth_of_rect",{breadth}"area_of_rect",{area}
